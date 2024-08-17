37482f3a9f5fb6d2e875088430e7dbfc3b1e3b0e
TrietMinh23 <npmtriet2k3@gmail.com>
2024-08-17 13:26:06 +0700

    fe/feat: Add react-icons and react-slick dependencies

---
+++ client/src/types/course.type.d.ts
@@ -0,0 +1,13 @@
+interface Course {
+    id: number;
+    title: string;
+    subcategory_id: string;
+    image: string;
+    // price: string;
+    // star: number;
+    // view: number;
+    // href: string;
+    // imageSrc: string;
+    // imageAlt: string;
+  }
+  
