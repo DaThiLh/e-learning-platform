f4db16fc8954683b87b085728880a403637d495f
TrietMinh23 <npmtriet2k3@gmail.com>
2024-08-25 20:47:57 +0700

    feat: Add item and section entities and DTOs

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,9 +5,11 @@ import { CoursesModule } from './courses/courses.module';
 import { CourseobjectivesModule } from './courseobjectives/courseobjectives.module';
 import { CoursehighlightModule } from './coursehighlight/coursehighlight.module';
 import { CourseinstructorsModule } from './courseinstructors/courseinstructors.module';
+import { SectionsModule } from './sections/sections.module';
+import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({}), PrismaModule, CoursesModule, CourseobjectivesModule, CoursehighlightModule, CourseinstructorsModule],
+  imports: [ConfigModule.forRoot({}), PrismaModule, CoursesModule, CourseobjectivesModule, CoursehighlightModule, CourseinstructorsModule, SectionsModule, ItemsModule],
   controllers: [],
   providers: [],
 })
